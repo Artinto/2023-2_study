a9bcdf5334b05e2d5a92e24acebe7b7ec47ec0b2
MinJi Kim (Mindal) <108729047+mjkim0819@users.noreply.github.com>
2023-07-28 20:12:34 +0900

    Update [KMJ]폰켓몬.py

--- [코딩]/해시/폰켓몬/[KMJ]폰켓몬.py
+++ [코딩]/해시/폰켓몬/[KMJ]폰켓몬.py
@@ -1,6 +1,7 @@
 def solution(nums):
-    answer = len(set(nums))
-    if len(nums) // 2 > answer:
+    answer = len(set(nums)) # 중복된 종류를 제거한 포켓몬 수
+    move = len(nums) // 2 # 가져갈 수 있는 포켓몬 수
+    if move > answer:
         return answer
     else:
-        return len(nums) // 2
+        return move
