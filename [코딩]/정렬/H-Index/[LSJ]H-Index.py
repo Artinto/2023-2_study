0fe096905c7125bc49917b8d0b88dfb87170434e
sj990710 <127752372+sj990710@users.noreply.github.com>
2023-09-01 20:58:37 +0900

    Update [LSJ]H-Index.py

--- [코딩]/정렬/H-Index/[LSJ]H-Index.py
+++ [코딩]/정렬/H-Index/[LSJ]H-Index.py
@@ -4,3 +4,4 @@ def solution(citations):
         if i >= citation: # h번 이상 인용된 논문의 수가 h 이상인 경우
             return i
     return len(citations) # 반복문이 끝까지 진행됐는데도 답이 안 나온 경우: 모든 논문이 0회 이상 인용됐으므로 H-index = len(citation)
+    # ex) [10, 9, 8, 7, 6] => [[0,10], [1,9], [2,8], [3,7], [4,6] => 논문 수인 5가 답. 5번 이상 인용된 논문의 수가 5개.
