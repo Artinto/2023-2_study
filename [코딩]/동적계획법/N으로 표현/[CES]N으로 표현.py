d8cf7abeb58f6a7df3d7f837cbcb69f5f776b616
ces0o <127365253+ces0o@users.noreply.github.com>
2023-10-30 18:33:59 +0900

    Update [CES]N으로 표현.py

--- [코딩]/동적계획법/N으로 표현/[CES]N으로 표현.py
+++ [코딩]/동적계획법/N으로 표현/[CES]N으로 표현.py
@@ -1,40 +1,36 @@
-# N이 i개 있는 만큼 set을 만들어주기
-# dp[i]는 dp[i-1]의 구성요소로 사칙 연산이 구성 되어있다
-# 최종 dp[i] set에서 number가 존재하면 i를 반환
-# 발견 못하면 -1 출력
 def solution(N, number):
-    answer = -1
-    dp = []
-    # 이 리스트는 동적 프로그래밍(DP)의 중간 결과를 저장할 목적으로 사용
-    for i in range(1,9):
-    # i는 N의 개수
-        A_case = set()
-        # 가능한 모든 숫자 조합을 저장할 용도로 사용
-        C_number = int(str(N)*i)
-        # N을 i만큼 곱한 값을 정수 형태로 C_number에 저장
-        A_case.add(C_number)
-        # A_case 집합에 C_number를 추가. 이것은 현재 i개의 N만 사용해서 만들 수 있는 숫자를 저장
-        
-            for j in range(0, i-1):
-            # j개를 사용해서 만든 값 
-                for op1 in dp[j]:
-                # dp 리스트에서 이전 단계에서 계산한 j개의 N을 사용한 경우의 수를 반복
-                    A_case.add(op1-op2)
-                    A_case.add(op1+op2)
-                    A_case.add(op1*op2)
-                    # A_case 집합에 op1과 op2 간의 뺄셈, 덧셈, 곱셈 결과를 추가
-                    if op2 != 0:
-                        A_case.add(op1//op2)
-                        # 만약 op2가 0이 아니라면, 나눗셈 결과를 A_case에 추가
-            if number in A_case:
-                # 목표 숫자인 number가 A_case 집합 안에 있다면,목표로 하는 숫자를 만들 수 있는 경우
-                answer = i
-                # answer 변수를 i로 설정하고, 이것은 N을 몇 번 사용하여 목표 숫자를 만들 수 있는지를 나타냄
-                break
-                # 목표 숫자를 만들었으므로 루프를 종료
-            dp.append(A_case)
-            # 이번 루프에서 계산한 A_case 집합을 dp 리스트에 추가
-        return answer
-        # answer 값 반환 (최종 결과 도출)
-                        
+    dp = [set() for i in range(9)] 
+    # 사용 횟수에 따라 가능한 숫자를 담을 리스트 dp를 생성
+    # dp리스트는 1~8까지의 index를 가진다
+    for i in range(1, 9): 
+    # N의 개수를 1부터 8까지 증가시키는 루프
+        dp[i].add(int(str(N)*i)) 
+        # N을 i번 반복한 값을 dp[]집합에 추가
+        for j in range(i//2 + 1):
+        # 이중 루프를 시작하여 j개의 N을 사용한 경우를 고려
+        # i//2를 사용하는 이유는 중복되는 연산을 피하기 위해서
+            for first in dp[j]:
+            # 집합에서 j개의 N을 사용한 경우의 수 반복
+                for second in dp[i-j]:
+                # 집합에서 나머지개의 N을 사용한 경우의 수 반복
+                    dp[i].add(first + second)
+                    dp[i].add(first - second)
+                    dp[i].add(second - first)
+                    dp[i].add(first * second)
+                    # 덧셈,뺄셈,뺄셈(반대),곱셈 결과 추가
+                    if second != 0 :
+                    # 만약 second가 0이 아니라면
+                        dp[i].add(first // second)
+                        # 나눗셈 결과를 추가
+                    if first != 0 :
+                    # 만약 first가 0이 아니라면    
+                        dp[i].add(second // first)
+                        # 나눗셈 결과를 추가
+                    
+        if number in dp[i]:
+        # 만약 목표숫자인 number가 dp 집합 안에 있다면
+            return i
+            # i를 return
+    return -1
+    # 목표숫자가 없다면 -1를 반환
                 
