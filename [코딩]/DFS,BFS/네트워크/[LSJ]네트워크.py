800a8cb6bc0454ed7472f44182781b66ecdebd96
sj990710 <127752372+sj990710@users.noreply.github.com>
2023-11-12 22:43:36 +0900

    Update [LSJ]네트워크.py

--- [코딩]/DFS,BFS/네트워크/[LSJ]네트워크.py
+++ [코딩]/DFS,BFS/네트워크/[LSJ]네트워크.py
@@ -1,8 +1,8 @@
 def solution(n, computers):
     answer = 0
-    visited = [0 for _ in range(n)]
+    visited = [0 for _ in range(n)] # 다른 컴퓨터와의 관계 검사 여부
 
-    def dfs(computer):
+    def dfs(computer): 
         visited[computer] = 1
         for peer in range(n):
             if computers[computer][peer] == 1 and visited[peer] == 0:
