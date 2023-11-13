702812fc72ccbdf56de3303b9caa307cc2d6e457
sj990710 <127752372+sj990710@users.noreply.github.com>
2023-11-13 17:38:30 +0900

    Update [LSJ]게임_맵_최단거리.py

--- [코딩]/DFS,BFS/게임 맵 최단거리/[LSJ]게임_맵_최단거리.py
+++ [코딩]/DFS,BFS/게임 맵 최단거리/[LSJ]게임_맵_최단거리.py
@@ -7,15 +7,15 @@ def solution(maps):
     queue.append([0, 0]) # 초기 좌표 설정
     while queue: 
         x, y = queue.popleft() # x=0, y=0 . queue 비게 됨
-        for i in range(4):
+        for i in range(4): # 상하좌우로 1칸씩 움직여봄
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < len(maps) and 0 <= ny < len(maps[0]):
-                if maps[nx][ny] == 1:
-                    maps[nx][ny] = maps[x][y] + 1
-                    queue.append([nx, ny])
-    if maps[-1][-1] > 1:
-      answer = maps[-1][-1] 
+            if 0 <= nx < len(maps) and 0 <= ny < len(maps[0]): # 움직여진 곳이 지도 내의 장소 내에 있고 
+                if maps[nx][ny] == 1: # 움직일 곳이 아직 안 지나간 '길'이면
+                    maps[nx][ny] = maps[x][y] + 1 # 거리 +1
+                    queue.append([nx, ny]) # 현 좌표 입력
+    if maps[-1][-1] > 1: # 이동 완료했다면
+      answer = maps[-1][-1] # 거리 반환
     else:
-      answer = -1
+      answer = -1 # 막혔다면 -1 반환
     return answer
