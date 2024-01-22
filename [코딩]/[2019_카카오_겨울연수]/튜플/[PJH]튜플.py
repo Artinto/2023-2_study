e9705bb551c67482d7b47a45200d105cb5ce21e6
JooH <59814944+ParkJooHyeung@users.noreply.github.com>
2024-01-22 21:30:22 +0900

    Create [PJH]튜플.py

---
+++ [코딩]/[2019_카카오_겨울연수]/튜플/[PJH]튜플.py
@@ -0,0 +1,17 @@
+def solution(s):
+    s=s[1:-1]
+    s=s.split("},")
+    for i in range(len(s)):
+        if i==((len(s))-1):
+            s[i]=(s[i])[1:-1]
+        else:
+            s[i]=(s[i])[1:]
+    s=sorted(s, key=lambda x:len(x))
+    rs=[]
+    for i in s:
+        a=i.split(',')
+        for j in a:
+            if int(j) not in rs:
+                print(j)
+                rs.append(int(j))
+    return rs
