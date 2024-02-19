2c6d2537c0fb59f64d351753d080b795d92ec470
MinJi Kim (Mindal) <108729047+mjkim0819@users.noreply.github.com>
2024-02-19 19:28:36 +0900

    Update [KMJ] 표_편집.py

--- [코딩]/2020_카카오_인턴십/표_편집/[KMJ] 표_편집.py
+++ [코딩]/2020_카카오_인턴십/표_편집/[KMJ] 표_편집.py
@@ -1,26 +1,28 @@
-# 틀린 코드
+# 효율성 테스트 통과 X
 
 def solution(n, k, cmd):
     answer = ''
-    curser = k
-    num = list(range(n))
-    delete = []
-    result = ["O" for i in range(n)]
+    cursor = k
+    num = list(range(n)) # [0, 1, 2, 3, 4, 5] -> cursor : 3, C -> [0, 1, 2, 4, 5]
+    delete = [] # 삭제된 3 추가 [3]
+    result = ["O" for i in range(n)] # ["O", "O", "O", "X", "O", "O"]
     
     for s in cmd:
         if s[0] == "D":
-            curser += int(s[2:])
+            cursor += int(s[2:]) # 1의 자리 숫자가 아닐 수 있으므로 s[2]가 아니라 s[2:]
         elif s[0] == "U":
-            curser -= int(s[2:])
-        elif s[0] == "C":
-            result[num[curser]] = "X"
-            delete.append(num.pop(curser))
+            cursor -= int(s[2:])
+        elif s[0] == "C":   
+            result[num[cursor]] = "X"
+            if num[cursor] == num[-1]:
+                cursor -=1
+                delete.append(num.pop(cursor+1))
+            else:
+                delete.append(num.pop(cursor))
         elif s[0] == "Z":
             result[delete[-1]] = "O"
-            if num[curser] < delete[-1]:
-                curser -= 1
-            else:
-                curser += 1
+            if num[cursor] > delete[-1]:
+                cursor += 1
             num.append(delete.pop())
             num.sort()
     answer = ''.join(map(str, result))
