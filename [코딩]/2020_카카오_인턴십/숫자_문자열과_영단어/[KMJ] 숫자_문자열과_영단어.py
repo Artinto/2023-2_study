9a74f510cc2ff4428df46d3f5606323955bbf2da
Seungbeom oh <osr0912@gmail.com>
2024-02-29 18:11:20 +0900

    remove: the moved files

--- [코딩]/2020_카카오_인턴십/숫자_문자열과_영단어/[KMJ] 숫자_문자열과_영단어.py
+++
@@ -1,7 +0,0 @@
-def solution(word):
-    dic = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-    for i in dic:
-        if i in word:
-            word = word.replace(i, str(dic.index(i)))
-    answer = int(word)
-    return answer
